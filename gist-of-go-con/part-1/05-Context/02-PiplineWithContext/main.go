de2b7c25c558b88c12737c44d8c66fff06ebd8d2
Jegadeesh SSRA <jegadeesh0003@gmail.com>
2026-09-19 14:38:02 +0530

    gist-of-go:05 - Context(Cancel Propagation within Pipeline)

---
+++ gist-of-go-con/part-1/05-Context/02-PiplineWithContext/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func produce(ctx context.Context, start int, stop int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for i := start; i <= stop; i++ {
+			select {
+			case out <- i:
+			case <-ctx.Done():
+				fmt.Println("Producer Stopped")
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func process(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case n := <-in:
+				time.Sleep(10 * time.Millisecond)
+				select {
+				case out <- (n * 2):
+				case <-ctx.Done():
+					fmt.Println("Processor Stopped")
+					return
+				}
+			case <-ctx.Done():
+				fmt.Println("Processor Stopped")
+				return
+			}
+		}
+	}()
+	return out
+}
+func consume(ctx context.Context, in <-chan int) {
+	go func() {
+		for {
+			select {
+			case val := <-in:
+				fmt.Println("Final Val -", val)
+			case <-ctx.Done():
+				fmt.Println("Consumer Stopped")
+				return
+			}
+		}
+	}()
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	producer := produce(ctx, 1, 100)
+	processer := process(ctx, producer)
+	consume(ctx, processer)
+
+	time.Sleep(100 * time.Millisecond)
+	cancel()
+	time.Sleep(5 * time.Second)
+}
